7fb2e062173a3192b320292271844b01f11dc927
CherryHou <71437901+CherryHou@users.noreply.github.com>
2021-12-13 19:19:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def upload():
     return render_template("index.html", msg = msg)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
